8ce60a4f6c3630f49617051603c4c37c283e6eff
Negro_13 <141974651+Negro-13@users.noreply.github.com>
2025-03-31 16:49:43 -0300

    Delete Python-5 directory

--- Python-5/Ej2.py
+++
@@ -1,8 +0,0 @@
-def convertir(temp):
-    tempF = (temp * 9/5) + 32
-    print(f'La convercion de {temp} Celsius a Fahrenheit es {tempF} Fahrenheit')
-if __name__ == "__main__":
-    print('Ingrese una temperatura para pasar de Celsios a Fahrenheit')
-    temp = float(input())
-    convertir(temp)
-
